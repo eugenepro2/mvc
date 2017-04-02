37ed4a240717b6814e937db6df34bbd174c43115
Your Name <you@example.com>
2017-04-02 22:31:14 +0300

    upload

---
+++ app.js
@@ -0,0 +1,45 @@
+var express = require('express');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine','pug');
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+	res.render('home');
+});
+
+app.get('/about', function(req, res) {
+	var randomFortune =
+		fortunes[Math.floor(Math.random() * fortunes.length)];
+	res.render('about', { fortune: randomFortune });
+});
+
+
+var fortunes = [
+	"Победи свои страхи, или они победят тебя.",
+	"Рекам нужны истоки.",
+	"Не бойся неведомого.",
+	"Тебя ждет приятный сюрприз.",
+	"Будь проще везде, где только можно.",
+];
+
+// Обобщенный обработчик 404 (промежуточное ПО)
+app.use(function(req, res, next){
+	res.status(404);
+	res.render('404');
+});
+
+// Обработчик ошибки 500 (промежуточное ПО)
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+	console.log( 'Express запущен на http://localhost:' +
+	app.get('port') + '; нажмите Ctrl+C для завершения.' );
+});
